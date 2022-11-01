eab3c12a25bd383f1a8152fe3fe1053b0025fcf1
Afrooz Shaddel <afrooz880@gmail.com>
2022-11-01 22:08:07 +0330

    chang food1

---
+++ item.js
@@ -0,0 +1,404 @@
+const foodItem=[
+    {
+    id: 1,
+    name: 'Ambur ',
+    category : 'Italian Food',
+    rating : 4.3,
+    price: 13,
+    img:'images/italian food/210211142532-18-classic-italian-dishes-super-tease.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi ',
+    category : 'Italian Food',
+    rating : 4.3,
+    price: 15,
+    img: 'images/italian food/content10735.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg ',
+    category : 'Italian Food',
+    rating : 4.3,
+    price: 18,
+    img: 'images/italian food/Famous-Italian-dishes.webp',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Goan Fish',
+    category : 'Italian Food',
+    rating : 4.3,
+    price: 15,
+    img: 'images/italian food/Homemade-Ground-Beef-Lasagna.png',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Mutton',
+    category : 'Italian Food',
+    rating : 4.3,
+    price: 10,
+    img: 'images/italian food/images.jfif',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Kamrup',
+    category : 'Italian Food',
+    rating : 4.3,
+    price: 12,
+    img: 'images/italian food/learn-about-italian-cuisine.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Kashmir',
+    category : 'Italian Food',
+    rating : 4.3,
+    price: 13,
+    img: 'images/italian food/vegan-cannoli.1.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Memoni ',
+    category : 'Italian Food',
+    rating : 4.3,
+    price: 20,
+    img: 'images/italian food/474ea8ff-9b8c-4281-bba3-370ab9ac0756-NDN_0518_JA_OSTERIA-CAPRI_09.webp',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Mughlai ',
+    category : 'Italian Food',
+    rating : 4.3,
+    price: 15,
+    img: 'images/italian food/pizza.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: ' Roast',
+    category : 'Mexican food',
+    rating : 4.3,
+    price: 11,
+    img: 'images/mexcican food/2ac65d712a776381c73406f97192e9aa.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: ' Curry',
+    category : 'Mexican food',
+    rating : 4.3,
+    price: 10,
+    img: 'images/mexcican food/10best-mexicanrestaurants-update-mi-dia_55_660x440.webp',
+    quantity: 1
+},
+{
+    id: 12,
+    name: ' Do Pyaza',
+    category : 'Mexican food',
+    rating : 4.3,
+    price: 14,
+    img: 'images/mexcican food/15319269_1282267388514115_2001369793558857061_n.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Chicken Masala',
+    category : 'Mexican food',
+    rating : 4.3,
+    price: 12,
+    img: 'images/mexcican food/Capture.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Handi ',
+    category : 'Mexican food',
+    rating : 4.3,
+    price: 17,
+    img: 'images/mexcican food/Enchiladas-is-a-traditional-food-from-Mexico.webp',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Murgh Musallam',
+    category : 'Mexican food',
+    rating : 4.3,
+    price: 20,
+    img: 'images/mexcican food/Steak-Fajitas-5-660x500.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Matar ',
+    category : 'Sandwiches Food',
+    rating : 4.3,
+    price: 15,
+    img: 'images/sandwich/0f5dbed0-0371-4686-9723-0116f49aaf6d-washington.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Palak ',
+    category : 'Sandwiches Food',
+    rating : 4.3,
+    price: 10,
+    img: 'images/sandwich/35AHE43HQFE3CD4KV52TZGUJGY.webp',
+    quantity: 1
+},
+{
+    id: 18,
+    name: ' Butter Masala',
+    category : 'Sandwiches Food',
+    rating : 4.3,
+    price: 15,
+    img: 'images/sandwich/247WallSt.com-247WS-680368-imageForEntry1-erv.webp',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: ' Do Pyaza',
+    category : 'Sandwiches Food',
+    rating : 4.3,
+    price: 12,
+    img: 'images/sandwich/american-hot-cheese-sandwich-homemade-grilled-cheese-sandwich-breakfast_2829-3521.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Hyderabadi ',
+    category : 'Sandwiches Food',
+    rating : 4.3,
+    price: 8,
+    img: 'images/sandwich/hoagie-sub-grinder©iStock.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: ' Lababdar',
+    category : 'Sandwiches Food',
+    rating : 4.3,
+    price: 7,
+    img: 'images/sandwich/images.jfif',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Shahi ',
+    age: '32',
+    category : 'Sandwiches Food',
+    rating : 4.3,
+    price: 5,
+    img: 'images/sandwich/best-sandwiches-blt.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Navratan Korma',
+    category : 'Sochi Food',
+    rating : 4.3,
+    price: 8,
+    img: 'images/sochi/1-Salmon-Nigiri-1-1-of-1.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Veg Jalfrezi',
+    category : 'Sochi Food',
+    rating : 4.3,
+    price: 7,
+    img: 'images/sochi/download (1).jfif',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Veg ',
+    category : 'Sochi Food',
+    rating : 4.3,
+    price: 5,
+    img: 'images/sochi/download (2).jfif',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Veg Curry',
+    category : 'Sochi Food',
+    rating : 4.3,
+    price: 7,
+    img: 'images/sochi/download.jfif',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Veg Kolhapur',
+    category : 'Sochi Food',
+    rating : 4.3,
+    price: 10,
+    img: 'images/sochi/grilled-salmon-sushi-roll-japanese-food_1339-101449.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Veg Masala',
+    category : 'Sochi Food',
+    rating : 4.3,
+    price: 4,
+    img: 'images/sochi/table-sushi-roll-food-fish-philadelphia-japanese-salmon-delicious-sushi-rice-cucumber-meal-traditional-wasabi-fresh-healthy-gourmet-raw-cuisine_211786-1307.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Veg Pakora',
+    category : 'Sochi Food',
+    rating : 4.3,
+    price: 4,
+    img: 'images/sochi/RFO-1400x919-SushiSandwich-mini-8f79f22c-2c62-4468-ab71-7a3e107c8e21-0-1400x919.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Momos',
+    category : 'Spain Food',
+    rating : 4.3,
+    price: 8,
+    img: 'images/spain food/10-amazing-facts-about-Spanish-food.jfif',
+    quantity: 1
+},
+{
+    id: 31,
+    name: ' Manchurian',
+    category : 'Spain Food',
+    rating : 4.3,
+    price: 7,
+    img: 'images/spain food/1344463-pintxos-san-sebastin.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Chili ',
+    category : 'Spain Food',
+    rating : 4.3,
+    price: 5,
+    img: 'images/spain food/6937561683_664bc1cce1_b.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Chowmein',
+    category : 'Spain Food',
+    rating : 4.3,
+    price: 16,
+    img: 'images/spain food/filters_focal(null).webp',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Spring Roll',
+    category : 'Spain Food',
+    rating : 4.3,
+    price: 14,
+    img: 'images/spain food/gambas-1419c7f.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Szechuan',
+    category : 'Spain Food',
+    rating : 4.3,
+    price: 10,
+    img: 'images/spain food/images (1).jfif',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Fried Rice',
+    category : 'Spain Food',
+    rating : 4.3,
+    price: 8,
+    img: 'images/spain food/lEspai-del-Peix-Anchovies-Girona-Food-Guide-via-2foodtrippers.jpg.webp',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Butter Masala Dosa',
+    category : 'Desserts',
+    rating : 4.3,
+    price: 18,
+    img: 'images/deseart/5da794101aeb3636e5e340d4.w800.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Idli',
+    category :'Desserts',
+    rating : 4.3,
+    price: 20,
+    img: 'images/deseart/22-tiramisu_t750x550.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Masala Dosa',
+    category : 'Desserts',
+    rating : 4.3,
+    price: 12,
+    img: 'images/deseart/ce5d21e6ec05453b9c6217f1e5d4252c.jpeg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Mysore Bonda',
+    category : 'Desserts',
+    rating : 4.3,
+    price: 10,
+    img: 'images/deseart/canadian_living-_-c6aa11c2ff328f3af7292847689862b719a5b814-_-03.jpeg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Onion Uttapam',
+    category : 'Desserts',
+    rating : 4.3,
+    price: 15,
+    img: 'images/deseart/dessert-making-class-five-delicious-desserts-0-2-portrait-big.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Plain Dosa',
+    category : 'Desserts',
+    rating : 4.3,
+    price: 40,
+    img: 'images/deseart/mini-pumpkin-cheesecakes-6.jpg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Rava Uttapam',
+    category : 'Desserts',
+    rating : 4.3,
+    price: 25,
+    img: 'images/deseart/delicious-desserts-that-reduce-stress.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Sambhar Vada',
+    category : 'Desserts',
+    rating : 4.3,
+    price: 34,
+    img: 'images/deseart/carlota-1-1024x599 (1).jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
